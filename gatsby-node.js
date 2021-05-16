0d0027fc40ac4903c18265568dc97a5c768af29f
Ashlyn Baum <ashlyn@traject.nz>
2021-05-16 17:27:46 +1200

    Add notion CLI, begin to remove apollo

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,37 +3,46 @@ const { WebSocketLink } = require("@apollo/client/link/ws")
 const { getMainDefinition } = require("@apollo/client/utilities")
 const WebSocket = require("ws")
 const fetch = require("node-fetch")
-const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
+const { createRemoteFileNode } = require("gatsby-source-filesystem")
+const { Client } = require('@notionhq/client');
 
 
-exports.onPreInit = () => console.log("Loaded gatsby-source-notion-api")
+
+// exports.onPreInit = () => console.log("Loaded gatsby-source-notion-api")
+exports.onPreInit = (_, pluginOptions) => {
+  console.log(
+    `Your notion key is: ${pluginOptions.notionKey} \n Your database ID is ${pluginOptions.tableID}`
+  )
+}
 
 // constants for your GraphQL Post and Author types
 const POST_NODE_TYPE = `Post`
 
-const client = new ApolloClient({
-    link: split(
-      ({ query }) => {
-        const definition = getMainDefinition(query)
-        return (
-          definition.kind === "OperationDefinition" &&
-          definition.operation === "subscription"
-        )
-      },
-      new WebSocketLink({
-        uri: `ws://gatsby-source-plugin-api.glitch.me/`,
-        options: {
-          reconnect: true,
-        },
-        webSocketImpl: WebSocket,
-      }),
-      new HttpLink({
-        uri: "https://gatsby-source-plugin-api.glitch.me/",
-        fetch,
-      })
-    ),
-    cache: new InMemoryCache(),
-  })
+
+// const client = new ApolloClient({
+//     link: split(
+//       ({ query }) => {
+//         const definition = getMainDefinition(query)
+//         return (
+//           definition.kind === "OperationDefinition" &&
+//           definition.operation === "subscription"
+//         )
+//       },
+//       new WebSocketLink({
+//         uri: `ws://gatsby-source-plugin-api.glitch.me/`,
+//         options: {
+//           reconnect: true,
+//         },
+//         webSocketImpl: WebSocket,
+//       }),
+//       new HttpLink({
+//         uri: "https://api.notion.com/v1/databases/${pluginOptions.tableID}",
+// 				credentials: "pluginOptions.notionKey",
+//         fetch,
+//       })
+//     ),
+//     cache: new InMemoryCache(),
+//   })
 
 	exports.sourceNodes = async ({
 		actions,
@@ -43,44 +52,50 @@ const client = new ApolloClient({
 		},
 		pluginOptions
 	) => {
-		const { createNode } = actions
-		console.log(pluginOptions.tableID) // tableID looks like 2eb7999026f1465ea58832e1620dcdb6
-		const { data } = await client.query({
-			query: gql`
-				query {
-					posts {
-						id
-						description
-						slug
-						imgUrl
-						imgAlt
-						author {
-							id
-							name
-						}
-					}
-					authors {
-						id
-						name
-					}
-				}
-			`,
-		})
+		// const { createNode } = actions
+		const notion = new Client({ auth: pluginOptions.notionKey });
+
+		(async () => {
+			const databaseId = pluginOptions.tableID;
+			const client = await notion.databases.retrieve({ database_id: databaseId });
+			console.log(client);
+		})()
+		// const { data } = await client.query({
+		// 	query: gql`
+		// 		query {
+		// 			posts {
+		// 				id
+		// 				description
+		// 				slug
+		// 				imgUrl
+		// 				imgAlt
+		// 				author {
+		// 					id
+		// 					name
+		// 				}
+		// 			}
+		// 			authors {
+		// 				id
+		// 				name
+		// 			}
+		// 		}
+		// 	`,
+		// })
 
 		// loop through data and create Gatsby nodes
-		data.posts.forEach(post =>
-			createNode({
-				...post,
-				id: createNodeId(`${POST_NODE_TYPE}-${post.id}`),
-				parent: null,
-				children: [],
-				internal: {
-					type: POST_NODE_TYPE,
-					content: JSON.stringify(post),
-					contentDigest: createContentDigest(post),
-				},
-			})
-		)
+		// data.posts.forEach(post =>
+		// 	createNode({
+		// 		...post,
+		// 		id: createNodeId(`${POST_NODE_TYPE}-${post.id}`),
+		// 		parent: null,
+		// 		children: [],
+		// 		internal: {
+		// 			type: POST_NODE_TYPE,
+		// 			content: JSON.stringify(post),
+		// 			contentDigest: createContentDigest(post),
+		// 		},
+		// 	})
+		// )
 
   return
 }
