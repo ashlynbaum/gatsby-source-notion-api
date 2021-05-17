688b7bd821daba209845788e611f8f5e4565cb84
Ashlyn Baum <ashlyn@traject.nz>
2021-05-17 15:35:30 +1200

    wip render nodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,31 +17,27 @@ exports.onPreInit = (_, pluginOptions) => {
 
 // constants for your GraphQL Post and Author types
 const POST_NODE_TYPE = `Post`
+const PAGE_NODE_TYPE = `Page`
 
-const createNodeId = {
-	
-}
+
 
 
 exports.sourceNodes = async ({
-	actions,
-	createContentDigest,
-	createNodeId,
-	getNodesByType,
-	},
-	pluginOptions
-) => {
-	// const { createNode } = actions
-	const notion = new Client({ auth: pluginOptions.notionKey });
-
-	(async () => {
+		actions,
+		createContentDigest,
+		createNodeId,
+		getNodesByType,
+		},
+		pluginOptions
+	) => {
+		const { createNode } = actions
+		const notion = new Client({ auth: pluginOptions.notionKey });
 		const databaseId = pluginOptions.tableID;
+			
 		const client = await notion.databases.retrieve({ database_id: databaseId });
-		console.log(client);
-	})();
-	(async () => {
-		const databaseId = pluginOptions.tableID;
-		const response = await notion.databases.query({
+		// console.log(client);
+
+		const database = await notion.databases.query({
 			database_id: databaseId,
 			// filter: {
 			// 	or: [
@@ -66,31 +62,11 @@ exports.sourceNodes = async ({
 			// 	},
 			// ],
 		});
-		console.log(response);
-	})();
+		// console.log(database);
 
 
-		// const { data } = await client.query({
-		// 	query: gql`
-		// 		query {
-		// 			posts {
-		// 				id
-		// 				description
-		// 				slug
-		// 				imgUrl
-		// 				imgAlt
-		// 				author {
-		// 					id
-		// 					name
-		// 				}
-		// 			}
-		// 			authors {
-		// 				id
-		// 				name
-		// 			}
-		// 		}
-		// 	`,
-		// })
+// update so that query matches client properties keys
+// [{properties: {key: value}}] 
 
 		// loop through data and create Gatsby nodes
 		// data.posts.forEach(post =>
@@ -107,27 +83,51 @@ exports.sourceNodes = async ({
 		// 	})
 		// )
 
+
+
+		// console.log(database.results);
+		database.results.forEach( page =>
+			createNode({
+				id: createNodeId(`${PAGE_NODE_TYPE}-${page.ID}`),
+				parent: null,
+				children: [],
+				internal: {
+					type: PAGE_NODE_TYPE,
+					content: JSON.stringify(page),
+					contentDigest: createContentDigest(page),
+				},
+			});
+
+			console.log(page);
+					//  grab page data
+					// create node per page
+
+
+		)
+
   return
 }
 
+// update Images to support remote images
+
   // called each time a node is created
-	exports.onCreateNode = async ({
-    node, // the node that was just created
-    actions: { createNode },
-    createNodeId,
-    getCache,
-  }) => {
-    if (node.internal.type === POST_NODE_TYPE) {
-      const fileNode = await createRemoteFileNode({
-        // the url of the remote image to generate a node for
-        url: node.imgUrl,
-        parentNodeId: node.id,
-        createNode,
-        createNodeId,
-        getCache,
-      })
-      if (fileNode) {
-        node.remoteImage___NODE = fileNode.id
-      }
-    }
-  }
+	// exports.onCreateNode = async ({
+  //   node, // the node that was just created
+  //   actions: { createNode },
+  //   createNodeId,
+  //   getCache,
+  // }) => {
+  //   if (node.internal.type === POST_NODE_TYPE) {
+  //     const fileNode = await createRemoteFileNode({
+  //       // the url of the remote image to generate a node for
+  //       url: node.imgUrl,
+  //       parentNodeId: node.id,
+  //       createNode,
+  //       createNodeId,
+  //       getCache,
+  //     })
+  //     if (fileNode) {
+  //       node.remoteImage___NODE = fileNode.id
+  //     }
+  //   }
+  // }
